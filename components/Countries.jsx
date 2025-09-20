ef8dc4b809e6bdc10114ec2c7c1193291d3ff125
Xubaear <xubaear@gmail.com>
2025-09-20 12:26:11 +0600

    added country info

--- components/Countries.jsx
+++ components/Countries.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import Country from './Country/Country';
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
@@ -7,6 +8,13 @@ const Countries = ({countriesPromise}) => {
     return (
         <div>
             <h1>In the countries: {countries.length}</h1>
+            {
+                countries.map(country=><Country 
+                key={country.cca3.cca3}
+                country={country}>
+
+                </Country>)
+            }
         </div>
     );
 };
